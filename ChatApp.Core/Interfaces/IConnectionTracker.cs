09233dfb81606e7a7f8e8b430dacf4f9f1742e2e
kaleab solomon <kaleab24gebreselassie@gmail.com>
2026-06-05 10:25:12 +0300

    Revert "creating project structure"
    This reverts commit 26a1c0bf68297dd0086fc1f8fc46c992d9bd01ba.

--- ChatApp.Core/Interfaces/IConnectionTracker.cs
+++
@@ -1,9 +0,0 @@
-namespace ChatApp.Core.Interfaces;
-
-public interface IConnectionTracker
-{
-    void AddConnection(string userId, string connectionId);
-    void RemoveConnection(string connectionId);
-    IReadOnlyList<string> GetConnections(string userId);
-    string? GetUserId(string connectionId);
-}
